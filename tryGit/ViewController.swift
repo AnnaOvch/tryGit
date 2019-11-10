85b30710fb4905e20222d5ad10e24c45847b4db7
Anna Ovchynnykova <ms.annabell29@gmail.com>
2019-11-10 14:26:24 +0200

    first commit

---
+++ tryGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tryGit
+//
+//  Created by Анна Овчинникова  on 11/10/19.
+//  Copyright © 2019 Анна Овчинникова . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
